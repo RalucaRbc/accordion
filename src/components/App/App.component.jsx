410b389861cc9eac51e73d1dc90a80e6c9deefaf
RalucaRbc <office.movmil@gmail.com>
2021-04-14 08:06:06 +0100

    Project setup

---
+++ src/components/App/App.component.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import data from "../../data";
+import SingleQuestion from "../Question/Question.component";
+
+export const App = () => {
+  return <h2>accordion project setup</h2>;
+};
---
+++ src/components/Question/Question.component.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+export const Question = () => {
+  return <h2>question component</h2>;
+};
